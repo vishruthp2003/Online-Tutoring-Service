478da9327860625bfb5bc8b4cb48807d053796ed
vishruthp2003 <115341957+vishruthp2003@users.noreply.github.com>
2022-11-18 06:28:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let menubtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+menubtn.onclick = () =>{
+    menubtn.classList.toggle('fa-time');
+}
